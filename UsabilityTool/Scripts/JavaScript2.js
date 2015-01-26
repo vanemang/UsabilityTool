cd15c64cf211f0241a2e2b15b14363590906b631
Vanessa <vmangion@gmail.com>
2015-01-27 00:39:14 +0100

    First Commit

---
+++ UsabilityTool/Scripts/JavaScript2.js
@@ -0,0 +1,53 @@
+﻿function requestCrossDomain(site, callback) {
+
+    // If no url was passed, exit.
+    if (!site) {
+        alert('No site was passed.');
+        return false;
+    }
+
+    // Take the provided url, and add it to a YQL query. Make sure you encode it!
+    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + site + '"') + '&format=xml&callback=cbFunc';
+
+    // Request that YSQL string, and run a callback function.
+    // Pass a defined function to prevent cache-busting.
+    $.getJSON(yql, function(data){
+        if(data.results[0]){
+            data = data.results[0].replace(/<script[^>]*>[\s\S]*?<\/script>/, '');
+
+            if(typeof callback === 'function'){
+                callback(data);
+            }
+        }
+        else throw new Error('Nothing returned');
+    });
+}
+
+
+//when document loads
+$(document).ready(function () {
+
+
+    //query string
+    //var qsArray = window.location.href.split('?');
+    //var qs = qsArray[qsArray.length - 1];
+
+    //if (qs != '' && qsArray.length > 1) {
+    //    loadPage('', qs);
+    //}    
+
+    //when the url textbox is used
+    $('form').submit(function () {
+        var path = $('#url input[type=text]').val();
+        if (path.substr(0, 7) !== 'http://' && path.substr(0, 8) !== 'https://' && path.substr(0, 7) !== 'file://') {
+            path = 'http://' + path;
+        }
+
+        requestCrossDomain(path, function (results) {
+            $('#textbox1').html(results);
+        });
+
+        return false;
+    });
+
+});
---
+++ UsabilityTool/StartingPage.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StartingPage.aspx.cs" Inherits="UsabilityTool._Default" %>
+
+
+<asp:Content runat="server" ID="container" ContentPlaceHolderID="MainContent">  
+    <script type= "text/javascript">
+
+
+        $(function EnterEvent(e) {
+            if (e.keyCode == 13) {
+                __doPostBack('<%=Button1%>', "");
+        }
+    });
+
+    </script>
+
+    <div id="url">
+        <asp:TextBox ID="URLBox" runat="server" placeholder="Test your own site... type the url and hit enter" onkeypress="return EnterEvent(event)"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" style="display:none" Text="Submit" OnClick="Button1_Click1" />
+        <br />
+        <br />
+        <asp:Label ID="Result" runat="server" Height="200px" Width="1005px"></asp:Label> 
+    </div>
+</asp:Content>
